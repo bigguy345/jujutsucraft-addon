167834ad9ba4d4915c6c0804e914eb32ccdf28f1
bigguy345 <monkeybar3200@gmail.com>
2024-09-04 11:39:19 +0300

    Increased RCT Strength when in Zone
    Decreased Curse Power consumed by RCT when in Zone

---
+++ src/main/java/com/jujutsucraftaddon/mixin/jujutsu/RCTEffectTickMixin.java
@@ -0,0 +1,42 @@
+package com.jujutsucraftaddon.mixin.jujutsu;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalDoubleRef;
+import net.mcreator.jujutsucraft.init.JujutsucraftModMobEffects;
+import net.mcreator.jujutsucraft.procedures.ReverseCursedTechniqueOnEffectActiveTickProcedure;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.LevelAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = ReverseCursedTechniqueOnEffectActiveTickProcedure.class, remap = false)
+public class RCTEffectTickMixin {
+
+    @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getCapability(Lnet/minecraftforge/common/capabilities/Capability;Lnet/minecraft/core/Direction;)Lnet/minecraftforge/common/util/LazyOptional;", ordinal = 0, shift = At.Shift.BEFORE))
+    private static void decreaseRCTEnergyDrain(LevelAccessor world, double x, double y, double z, Entity entity, CallbackInfo ci, @Local(name = "Amount") LocalDoubleRef Amount, @Local(name = "fatigue") LocalDoubleRef Fatigue) {
+
+        if (entity instanceof LivingEntity ent) {
+            MobEffectInstance zone = ent.getEffect(JujutsucraftModMobEffects.ZONE.get());
+            if (zone != null) {
+                int amplifier = zone.getAmplifier();
+                double ammount = Amount.get();
+                if (amplifier == 0)
+                    ammount = 5;
+                else if (amplifier == 1)
+                    ammount = 5;
+                else if (amplifier == 2)
+                    ammount = 5;
+                else if (amplifier == 3)
+                    ammount = 5;
+                else if (amplifier == 4)
+                    ammount = 5;
+                
+                Amount.set(ammount / Fatigue.get());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/jujutsucraftaddon/mixin/jujutsu/RCTKeyBindPressedMixin.java
@@ -0,0 +1,39 @@
+package com.jujutsucraftaddon.mixin.jujutsu;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalDoubleRef;
+import net.mcreator.jujutsucraft.init.JujutsucraftModMobEffects;
+import net.mcreator.jujutsucraft.procedures.KeyReverseCursedTechniqueOnKeyPressedProcedure;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = KeyReverseCursedTechniqueOnKeyPressedProcedure.class, remap = false)
+public class RCTKeyBindPressedMixin {
+
+    @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getServer()Lnet/minecraft/server/MinecraftServer;", ordinal = 1, shift = At.Shift.BEFORE))
+    private static void changeRCTStrengthWithZone(Entity entity, CallbackInfo ci, @Local(name = "level") LocalDoubleRef level) {
+
+        if (entity instanceof LivingEntity ent) {
+            MobEffectInstance zone = ent.getEffect(JujutsucraftModMobEffects.ZONE.get());
+
+            if (zone != null) {
+                int amplifier = zone.getAmplifier();
+                if (amplifier == 0)
+                    level.set(10);
+                else if (amplifier == 1)
+                    level.set(11);
+                else if (amplifier == 2)
+                    level.set(12);
+                else if (amplifier == 3)
+                    level.set(14);
+                else if (amplifier == 4)
+                    level.set(16);
+            }
+        }
+    }
+}
