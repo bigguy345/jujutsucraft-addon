2c0654334eb482a97bb66e913cb3c41cda102942
bigguy345 <monkeybar3200@gmail.com>
2024-09-16 16:53:43 +0300

    Added Cooldown storage framework to JujutsuData + DASH level

--- src/main/java/com/jujutsucraftaddon/capabilities/data/JujutsuData.java
+++ src/main/java/com/jujutsucraftaddon/capabilities/data/JujutsuData.java
@@ -5,6 +5,7 @@ import com.jujutsucraftaddon.capabilities.ModCapabilities;
 import com.jujutsucraftaddon.effects.IMobEffectInstance;
 import com.jujutsucraftaddon.network.PacketHandler;
 import com.jujutsucraftaddon.network.packet.SyncJujutsuData;
+import com.jujutsucraftaddon.skill.DashSkill;
 import com.jujutsucraftaddon.utility.AdvancementUtil;
 import com.jujutsucraftaddon.utility.ValueUtil;
 import net.mcreator.jujutsucraft.init.JujutsucraftModMobEffects;
@@ -23,11 +24,15 @@ import net.minecraftforge.common.util.LazyOptional;
 
 public class JujutsuData {
     public Player player;
-    public float blackFlashChance = -1, blackFlashDamageMulti = 4;
-    public boolean landedFirstBlackFlash, canHealOthers;
-    public int toHealID = -1;
     public JujutsucraftModVariables.PlayerVariables data;
     public Levels levels = new Levels(this);
+    public Cooldowns cooldowns = new Cooldowns(this);
+
+    public float blackFlashChance = -1, blackFlashDamageMulti = 4;
+    public boolean landedFirstBlackFlash, canHealOthers, infinityOn;
+    public int toHealID = -1;
+
+    public DashSkill currentDash;
 
     public JujutsuData() {
     }
@@ -37,17 +42,30 @@ public class JujutsuData {
         data = player.getCapability(JujutsucraftModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(null);
     }
 
+    public void tick() {
+        cooldowns.tick();
+
+        if (player.tickCount % 10 == 0)
+            JujutsuData.get(player).syncTracking();
+    }
+
     public Tag writeNBT() {
         CompoundTag nbt = new CompoundTag();
+        nbt.put("levels", levels.writeNBT());
+        nbt.put("cooldowns", cooldowns.writeNBT());
+
         nbt.putFloat("blackFlashChance", blackFlashChance);
         nbt.putFloat("blackFlashMulti", blackFlashDamageMulti);
         nbt.putBoolean("landedFirstBlackFlash", landedFirstBlackFlash);
-
-        if (!player.level().isClientSide())
+        
+        if (!player.level().isClientSide()) {
             canHealOthers = AdvancementUtil.isDone((ServerPlayer) player, "jujutsucraftaddon:rct_others");
+            infinityOn = player.getPersistentData().getBoolean("infinity");
+        }
         nbt.putBoolean("canHealOthers", canHealOthers);
         nbt.putInt("toHealID", toHealID);
-        nbt.put("levels", levels.writeNBT());
+        nbt.putBoolean("infinityOn", infinityOn);
+
 
         return nbt;
     }
@@ -58,8 +76,10 @@ public class JujutsuData {
         blackFlashDamageMulti = nbt.getFloat("blackFlashMulti");
         landedFirstBlackFlash = nbt.getBoolean("landedFirstBlackFlash");
         canHealOthers = nbt.getBoolean("canHealOthers");
+        infinityOn = nbt.getBoolean("infinityOn");
         toHealID = nbt.getInt("toHealID");
-        levels.readNBT(tag);
+        levels.readNBT(nbt.getCompound("levels"));
+        cooldowns.readNBT(nbt.getCompound("cooldowns"));
     }
 
     public boolean tamedMahoraga(boolean isNotSummoned) {
@@ -116,10 +136,34 @@ public class JujutsuData {
         }
     }
 
-    public static class Levels {
-        public static float MAX_RCT_LEVEL = 200;
+    public static class Cooldowns {
         public JujutsuData parent;
-        private float RCT;
+        public int DASH;
+
+        public Cooldowns(JujutsuData parent) {
+            this.parent = parent;
+        }
+
+        public void tick() {
+            if (DASH > 0)
+                DASH--;
+        }
+
+        public Tag writeNBT() {
+            CompoundTag levels = new CompoundTag();
+            levels.putInt("dash", DASH);
+            return levels;
+        }
+
+        public void readNBT(Tag tag) {
+            CompoundTag nbt = ((CompoundTag) tag).getCompound("levels");
+            DASH = nbt.getInt("dash");
+        }
+    }
+
+    public static class Levels {
+        public JujutsuData parent;
+        private float RCT, DASH;
 
         public Levels(JujutsuData parent) {
             this.parent = parent;
@@ -134,6 +178,10 @@ public class JujutsuData {
             }
 
             return 1 + Math.min(Math.max(fatigue, 0) / 600, 10) * 0.4f;
+        }
+
+        public void incrementDashLevel(float amount) {
+            DASH = (float) Math.min(DASH + amount, Config.MAX_DASH_LEVEL.get());
         }
 
         public void incrementRCTLevel(float amount) {
@@ -162,7 +210,7 @@ public class JujutsuData {
             MobEffectInstance zone = parent.player.getEffect(JujutsucraftModMobEffects.ZONE.get());
             if (zone != null)
                 zoneAmp = (int) (zone.getAmplifier() * 1.5f);
-            
+
             return (int) ValueUtil.lerp(0, Config.MAX_RCT_LEVEL_AMPLIFIER.get(), RCT / Config.MAX_RCT_LEVEL.get()) + zoneAmp;
         }
 
@@ -170,15 +218,21 @@ public class JujutsuData {
             return RCT;
         }
 
+        public float getDashLevel() {
+            return DASH;
+        }
+
         public Tag writeNBT() {
             CompoundTag levels = new CompoundTag();
-            levels.putFloat("rctLevel", RCT);
+            levels.putFloat("rct", RCT);
+            levels.putFloat("dash", DASH);
             return levels;
         }
 
         public void readNBT(Tag tag) {
             CompoundTag nbt = ((CompoundTag) tag).getCompound("levels");
-            RCT = nbt.getFloat("rctLevel");
+            RCT = nbt.getFloat("rct");
+            DASH = nbt.getFloat("dash");
         }
     }
 }
